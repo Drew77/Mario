b140fd3d2e1040fcfe844ad27027e181f845bd3b
Drew <mrdrewy_@hotmail.com>
2018-08-05 18:49:30 +0100

    Added delay after selecting 2

--- src/index.js
+++ src/index.js
@@ -6,26 +6,39 @@ const _ = require('underscore');
 const board = document.querySelector('.board');
 const music = new Audio('/dist/audio/theme.mp3');
 const win = new Audio('/dist/audio/win.mp3');
+const mute = document.querySelector('.mute');
 
 
 const game = {
   tiles: ['1up', 'bowser', 'feather', 'flower', 'mario', 'mushroom', 'star', 'what',
     '1up', 'bowser', 'feather', 'flower', 'mario', 'mushroom', 'star', 'what'],
+  muted: false,
+  selected: [],
+  playing: false,
   watchTiles() {
     document.querySelector('.board').addEventListener('click', (e) => {
+      console.log(this.selected)
+      if (this.selected.length === 2) {
+        return;
+      }
+      this.justClicked = true;
+      setTimeout(() => {
+        this.justClicked = false;
+      }, 500);
       const tile = e.target.parentElement;
       const id = tile.dataset.id;
       const type = e.target.dataset.type;
       if (tile.classList.contains('hidden')) {
         this.displayTile(tile);
-        if (this.firstClick) {
-          this.firstClicked = tile;
-          this.firstClick = false;
-          this.firstType = type;
+        if (this.selected.length === 0) {
+          this.selected.push(tile);
         } else {
-          this.firstClick = true;
-          if (this.firstType === type) {
+          this.selected.push(tile);
+          if (this.selected[0].children[0].dataset.type === type) {
             this.matches += 1;
+            setTimeout(() => {
+              this.selected = [];
+            }, 1000);
             if (this.matches === 8) {
               this.wonGame();
             }
@@ -33,23 +46,40 @@ const game = {
           }
           setTimeout(() => {
             this.hideTile(tile);
-            this.hideTile(this.firstClicked);
+            this.hideTile(this.selected[0]);
+            this.selected = [];
           }, 1000);
         }
       }
     });
   },
+  mute() {
+    this.muted = !this.muted;
+    if (this.muted) {
+      music.pause();
+    } else if (this.playing) {
+      music.play();
+    }
+  },
   wonGame() {
-    music.pause();
-    win.play();
-    board.innerHTML = '<button class="start">Start Game!</button>';
+    if (!this.muted) {
+      win.play();
+    }
+    board.innerHTML = '<div class="winner">Finally!</div>';
+    setTimeout(() => {
+      board.innerHTML = '<button class="start">Play Again!</button>';
+    }, 5000);
   },
   startGame() {
     this.firstClick = true;
     this.firstType = null;
     this.firstClicked = null;
     this.matches = 0;
-    music.play();
+    this.playing = true;
+    if (!this.muted) {
+      music.play();
+    }
+
     this.watchTiles();
     this.makeBoard();
   },
@@ -78,3 +108,4 @@ document.body.addEventListener('click', (e) => {
   }
 });
 
+mute.addEventListener('click', () => game.mute());
